101c18ff49fd8835189524bd2c37d0638c17d9f4
cannonkissane <34813753+cannonkissane@users.noreply.github.com>
2024-05-03 15:20:54 -0700

    login page

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link'
 
 export default function Nav() {
   return (
@@ -6,11 +7,17 @@ export default function Nav() {
       <div className="w-11/12">
       <div className="flex max-w-[1800px] justify-between items-center h-[90px]">
         <div className="flex justify-center items-center">
+          <Link href="/">
         <Image src="img/SSLogo2.svg" alt="Stanford Studio" height={1} width={220}  />
+        </Link>
         </div>
         <div className='flex gap-4'>
+        <Link href="/submit">
             <div className="text-paper-3 py-3 w-[180px] bg-paper rounded-lg  border border-solid border-paper-3 text-center">Post My Project</div>
+            </Link>
+            <Link href="/login">
             <div className="text-paper py-3 w-[180px]  bg-cardinal rounded-lg font-bold text-center">Login</div>
+            </Link>
         </div>
       </div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Nav from "./nav";
 import Ranking from "./ranking";
 import Home from "./home";
+// import Login from "./login/login"
 
 export default function Page() {
   return (
     <main className="flex w-full flex-col font-urbanist">
             <Nav></Nav>
             <Home></Home>
+            {/* <Login></Login> */}
     </main>
   );
 }

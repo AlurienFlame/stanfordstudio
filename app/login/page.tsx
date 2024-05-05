570e9a68d01f3296587cc0bf392359374bca9bfe
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2024-05-05 13:42:09 -0700

    magic login link working

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,45 @@
+'use client';
 
+import { useState } from 'react';
+import { supabase } from '../supabaseClient';
 
 export default function Login() {
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+
+  const handleLogin = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    setLoading(true);
+    const { error } = await supabase.auth.signInWithOtp({ email });
+
+    if (error) {
+      alert(error.message);
+    } else {
+      alert('Check your email for the login link!');
+    }
+    setLoading(false);
+  };
+
   return (
     <main className="flex w-full flex-col font-urbanist">
-            Login
+      <form className="form-widget" onSubmit={handleLogin}>
+        <div>
+          <input
+            className="inputField"
+            type="email"
+            placeholder="Your email"
+            value={email}
+            required={true}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <button className={'button block'} disabled={loading}>
+            {loading ? <span>Loading</span> : <span>Send magic link</span>}
+          </button>
+        </div>
+      </form>
     </main>
   );
 }
